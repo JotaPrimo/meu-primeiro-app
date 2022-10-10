41c6bbc5324050fcc31b6e37be7b945379dd796e
JotaPrimo <gestald118@gmail.com>
2022-10-10 09:21:34 -0400

    62. ngForm + Selected

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
+  public listComida: Array<{comida: string, preco: number}> = [
+    { comida: 'X-Bacon', preco: 10 },
+    { comida: 'X-Salada', preco: 12  },
+    { comida: 'X-Tudo', preco: 14  }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
